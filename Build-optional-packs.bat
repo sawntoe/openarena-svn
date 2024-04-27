4363703677f4bd5ef507ed6f1eec4d31b0e5819e
Sean Teo <sawntoe@gmail.com>
2024-04-27 16:23:50 +0800

    temporary commit while I migrate svn history

---
+++ Build-optional-packs.bat
@@ -0,0 +1,18 @@
+
+zip -9r uncut.pk3 CREDITS
+
+zip -9r uncut.pk3 COPYING
+
+cd packs
+cd uncut
+..\..\zip -9r ../../uncut.pk3 * -x \*svn* -x *.exe
+
+
+
+cd..
+cd more_crosshairs
+..\..\zip -9r ../../more_crosshairs.pk3 * -x \*svn* -x *.exe
+
+
+
+cd..\..\
---
+++ missionpack/ui/theme_azure.h
@@ -0,0 +1,36 @@
+/*
+===========================================================================
+Copyright (C) 2007 Open Arena Team
+
+This file is part of Open Arena.
+
+Open Arena is free software; you can redistribute it
+and/or modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2 of the License,
+or (at your option) any later version.
+
+Open Arena is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Open Arena; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+===========================================================================
+*/
+
+#define MP_BACKGROUND "ui/assets/splash22.jpg" // background of the menu
+#define MP_SND_SELECT "ui/assets/select.wav" // confirm noise
+#define MP_SND_HOVER "ui/assets/hover.wav" // hover noise
+
+#define MP_TITLECOLOR 1 1 1 1 // Title colors
+#define MP_TEXTCOLOR  0.24 0.70 0.935 1 // regular color of menu button text
+#define MP_FOCUSCOLOR 0.34 0.8 0.835 1 // focus 
+#define MP_BOX_HEADERCOLOR 1 1 1 1 // for the text appearing above editboxes
+#define MP_SMALLCOLOR 0.25 0.45 0.85 1 // the smaller options colors
+
+#define MP_BOX_BORDER 	0.25 0.25 0.25 1
+#define MP_BOX_FORE 	0.01   0.5   1   1
+#define MP_BOX_BACK 	0.0 0.0 0.1 1
+#define MP_BOX_OUTLINE 	0.01   0.5   1   0.3
